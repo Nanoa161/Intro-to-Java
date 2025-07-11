6b2fd73eca19582a3d81735f265c17227d90b5f7
Nanoa161 <nanaadwoaowusuakyaw@gmail.com>
2025-07-11 15:53:51 +0000

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,7 @@
+public class Helloworld{
+    public static void main(String[] args) {
+        String name="Penguin";
+        int age=15;
+        System.out.println("Hello, my name is" +name+ "and I am "+age+" years old ");
+    }
+}
